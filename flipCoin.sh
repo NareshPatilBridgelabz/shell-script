6d9ac9bb68cbdd09ba4f59ef56d52aadd881a780
NareshPatilBridgelabz <nareshpatil.nrp@gmail.com>
2020-03-13 13:28:23 +0530

    [Naresh] Refactor. use BC library

---
+++ flipCoin.sh
@@ -0,0 +1,17 @@
+# Flip Coin and print percentage of Heads and Tails
+# Author: Naresh
+# Date: 13/03/2020
+len=10
+headtotal=0;
+tailtotal=0;
+for i in `seq $len`
+do
+	n=0.$(( RANDOM%100 ))
+	if echo "$n > 0.6" | bc -l | grep -q 1
+	then
+		((tailtotal++))
+	else
+		((headtotal++))
+	fi
+done
+headper=$()
