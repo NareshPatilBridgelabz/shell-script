83a22ce082618523939bcff823efc2f9f2b4e126
NareshPatilBridgelabz <nareshpatil.nrp@gmail.com>
2020-03-13 15:02:27 +0530

    [Naresh] Refactor. add percentage functionality

--- flipCoin.sh
+++ flipCoin.sh
@@ -1,6 +1,8 @@
+#!/bin/bash -x
 # Flip Coin and print percentage of Heads and Tails
 # Author: Naresh
 # Date: 13/03/2020
+
 len=10
 headtotal=0;
 tailtotal=0;
@@ -14,4 +16,7 @@ do
 		((headtotal++))
 	fi
 done
-headper=$()
+headper=$(echo "scale=2;$headtotal/10*100" | bc)
+echo $headper
+tailper=$(echo "scale=2;$tailtotal/10*100" | bc)
+echo $tailper
