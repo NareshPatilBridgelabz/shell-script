e2d84d41c9abc6181decdaef1030f1b5dc804dad
NareshPatilBridgelabz <nareshpatil.nrp@gmail.com>
2020-03-14 12:01:44 +0530

    [Naresh] Refactor. change the logic

---
+++ BasicPrograms/primeFactore.sh
@@ -0,0 +1,14 @@
+#!/bin/bash -x
+# Print the prime factors of number N.
+# Author: Naresh
+# Date: 13/03/2020
+read -p "Enter the number to find its prime factors " number
+for (( index=2; index<=$number; index++ ))
+do
+	while (( $number%$index==0 ))
+	do
+		primeFactor=$primeFactor$index" ";
+		number=$(($number/$index));
+	done
+done
+echo $primeFactor;
