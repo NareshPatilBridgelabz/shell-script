60d9b950c51bdbeb5e150509f270b968f4b1454c
Naresh28696 <nareshpatil.nrp@gmail.com>
2020-03-19 18:48:07 +0530

    [Naresh] odify.

--- logicalprograms/gambler.sh
+++ logicalprograms/gambler.sh
@@ -1,5 +1,5 @@
 #!/bin/bash -x
-# Write a program Quadratic.java to find the roots of the equation a*x*x + b*x + c.
+# Simulates a gambler who start with $stake and place fair $1 bets until he/she goes broke (i.e. has no money) or reach $goal. Keeps track of the number of times he/she wins and the number of bets he/she makes. Run the experiment N times, averages the results, and prints them out.
 # Author: Naresh
 # Date: 14/03/2020
 read -p "Enter the stake : " stake
@@ -9,12 +9,17 @@ read -p "Enter number of time : " time
 win=0
 loss=0
 tempTime=$time
+while [[ $stake -ge $goal ]]
+do
+	read -p "Enter goal more than your stack : " goal
+done
+
 while (( $time > 0 ))
 do
 stackRound=$stake
 	while (( stackRound < goal && stackRound > 0 ))
-	do
-		random=0.$(( RANDOM%10))
+	do	
+		random=0.$(( RANDOM%10 ))
 		if  (( $(echo "$random" | awk '{print ($1 < 0.5)}') ));
 		then
 			(( stackRound-- ))
@@ -25,12 +30,15 @@ stackRound=$stake
 
 	if ((stackRound == goal));
 	then
+		echo "won"
 		(( win++ ))
 	else
+		echo "loss"
 		(( loss++ ))
 	fi
-
+	
+	
 	(( time-- ))
 done
-echo "win percentage is $(echo $win $tempTime | awk '{print ($1/$2*100)}')"
-echo "loss percentage is $(echo $loss $tempTime | awk '{print ($1/$2*100)}')"
+echo "win percentage is $(echo $win $tempTime | awk '{print ($1/$2*100) }') %"
+echo "loss percentage is $(echo $loss $tempTime | awk '{print ($1/$2*100) }') %"
